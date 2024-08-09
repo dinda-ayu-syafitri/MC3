394b67968e8da6178f92a554713f6823dfc368a8
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2024-08-09 10:39:56 +0700

    Add: Initial Commit

---
+++ MC3/MC3App.swift
@@ -0,0 +1,17 @@
+//
+//  MC3App.swift
+//  MC3
+//
+//  Created by Dinda Ayu Syafitri on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
