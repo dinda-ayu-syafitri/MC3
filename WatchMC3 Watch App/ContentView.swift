d1b051c752106822dae9e36f58c654b155473a86
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2024-08-12 19:14:29 +0700

    Add: Watch Project

---
+++ WatchMC3 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchMC3 Watch App
+//
+//  Created by Dinda Ayu Syafitri on 12/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchMC3 Watch App/WatchMC3App.swift
@@ -0,0 +1,17 @@
+//
+//  WatchMC3App.swift
+//  WatchMC3 Watch App
+//
+//  Created by Dinda Ayu Syafitri on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchMC3_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
