c1598840752a4f6e2466bed6161154043f5c7323
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-08-19 21:49:25 +0700

    ada isi file ketinggalan

--- MC3/ContentView.swift
+++ MC3/ContentView.swift
@@ -5,13 +5,52 @@
 //  Created by Dinda Ayu Syafitri on 09/08/24.
 //
 
-import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+//     @State var reachable = "No"
+     @StateObject var watchConnector = iOSToWatchConnector()
+    
+//     var body: some View {
+//         VStack {
+//             Spacer()
+            
+//             if watchConnector.messageText == "sosAlert" {
+//                 Text(watchConnector.messageText)
+//             }
+            
+            
+//             Spacer()
+            
+//             Text("Reachable \(reachable)")
+            
+//             Button(action: {
+//                 if watchConnector.session.isReachable{
+//                     self.reachable = "Yes"
+//                 }
+//                 else{
+//                     self.reachable = "No"
+//                 }
+                
+//             }) {
+//                 Text("Update")
+//             }
+            
+//             Spacer()
+            
+            //            Image(systemName: "globe")
+            //                .imageScale(.large)
+            //                .foregroundStyle(.tint)
+            //            Text("Hello, world!")
+    @AppStorage(KeyUserDefaultEnum.status.toString) private var logStatus: Bool = false
+
     var body: some View {
         ZStack {
-            Text("Hello world")
+//            if logStatus {
+                HomeView()
+//            } else {
+//                LoginView()
+//            }
         }
         .padding()
     }
